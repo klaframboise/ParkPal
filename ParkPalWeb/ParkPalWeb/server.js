52f873510c0177b171f2792947d9a1da17ee85e0
sadhvi mehta <sadhvi.mehta@mail.mcgill.ca>
2018-02-05 18:34:48 -0500

    started basic web setup

--- ParkPalWeb/ParkPalWeb/server.js
+++ ParkPalWeb/ParkPalWeb/server.js
@@ -1,8 +1,20 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
+var fs = require('fs');
+var __dirname = "./frontendWeb/";
+var __path = "OfflineSched.html";
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+	fs.readFile(__dirname + __path, function (err, data) {
+		if (err) {
+			res.writeHead(404, { 'Content-type': 'text/plan' });
+			resp.write('Page Was Not Found');
+			res.end();
+		} else {
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			res.end();
+		}
+	});
 }).listen(port);
