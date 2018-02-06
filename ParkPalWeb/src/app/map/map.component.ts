705b40b24b5f471ef62eaf64ad09e48832c761de
Moose <mostafa.saadat@outlook.com>
2018-02-06 15:37:19 -0500

    Add own google map. Remove old map.

--- ParkPalWeb/src/app/map/map.component.ts
+++ ParkPalWeb/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const google: any;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -10,6 +12,12 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let mapProp = {
+        center: new google.maps.LatLng(45.504987, -73.577260),
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
   }
 
 }
