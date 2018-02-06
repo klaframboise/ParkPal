7c10b54232d798708a71a7d6d3c8a174ee559764
CPanaritis <christos.panaritis@mail.mcgill.ca>
2018-02-05 20:07:32 -0500

    components created

--- ParkPalWeb/src/app/app.module.ts
+++ ParkPalWeb/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MapComponent } from './map/map.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MapComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule
