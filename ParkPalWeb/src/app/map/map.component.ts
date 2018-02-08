6a05ae90f29469ee0ad604ed1359e0c4ad90792e
Moose <mostafa.saadat@outlook.com>
2018-02-08 16:07:20 -0500

    Show direction on map of dummy locations.

--- ParkPalWeb/src/app/map/map.component.ts
+++ ParkPalWeb/src/app/map/map.component.ts
@@ -13,11 +13,31 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     let mapProp = {
-        center: new google.maps.LatLng(45.504987, -73.577260),
+        center: new google.maps.LatLng(45.504386, -73.576659),
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    
+    let directionsDisplay = new google.maps.DirectionsRenderer();
+    let directionsService = new google.maps.DirectionsService();
+
+    directionsDisplay.setMap(map);
+
+    let start = "McGill University, Montreal";
+    let end = "Toronto";
+    var request = {
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function(result, status){
+      if (status == 'OK') {
+        directionsDisplay.setDirections(result);
+      } else {
+        console.log("no");
+      }
+    })
   }
 
 }
