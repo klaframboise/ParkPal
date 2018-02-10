08dd9bcadea7df8ab2a6a4690812d6b07c95a26b
Kevin <kelaframboise@gmail.com>
2018-02-10 16:21:29 -0500

    Revert "Revert "Updated downloadhelper to work with new embedded resources system""
    This reverts commit 6fb8a248079d9168f8521ccd2a89dfa4d7840037.

--- ParkPal/ParkPal.Android/MainActivity.cs
+++ ParkPal/ParkPal.Android/MainActivity.cs
@@ -18,7 +18,6 @@ namespace ParkPal.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
--- ParkPal/ParkPal/App.xaml.cs
+++ ParkPal/ParkPal/App.xaml.cs
@@ -12,7 +12,6 @@ namespace ParkPal
 		public App ()
 		{
 			InitializeComponent();
-
 			MainPage mainPage = new ParkPal.MainPage();
 		}
 
