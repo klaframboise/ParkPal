d677c32c8f4c99ff22ba257dcb1bc16e8627d76e
Kevin <kelaframboise@gmail.com>
2018-02-10 19:06:41 -0500

    Converted ScheduleSystem to singleton

--- ParkPal/ParkPal/ScheduleSystem/ScheduleSystem.cs
+++ ParkPal/ParkPal/ScheduleSystem/ScheduleSystem.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GTFS;
 
 namespace ParkPal.ScheduleSystem
 {
     class ScheduleSystem
     {
-     
+        private static ScheduleSystem instance;
+        private static GTFSFeed gtfs;
+
+        private ScheduleSystem()
+        {
+
+        }
+
+        public static ScheduleSystem Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ScheduleSystem();
+                }
+                return instance;
+            }
+        }
+
+        public static GTFSFeed Gtfs
+        {
+            get
+            {
+                return gtfs;
+            }
+        }
     }
 }
