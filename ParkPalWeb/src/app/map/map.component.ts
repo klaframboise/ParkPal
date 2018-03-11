b195fae605e7a88fdfdee3af7327ffaf5b2aa996
Moose <mostafa.saadat@outlook.com>
2018-03-11 11:33:35 -0400

    Park and ride feature implemented on web

--- ParkPalWeb/src/app/map/map.component.ts
+++ ParkPalWeb/src/app/map/map.component.ts
@@ -25,6 +25,7 @@ export class MapComponent implements OnInit {
   map:any;
   directionsService: google.maps.DirectionsService;
   directionsDisplay: google.maps.DirectionsRenderer;
+  directionsDisplayTwo: google.maps.DirectionsRenderer;
 
   stations: Station[];
 
@@ -49,11 +50,13 @@ export class MapComponent implements OnInit {
     document.getElementById("routeoptions").innerHTML = "";
 
     let directionsPanel = document.getElementById("routeoptions");
+    let directionsPanel2 = document.getElementById("routeoptions2");
 
     
 
     
     this.directionsDisplay.setPanel(directionsPanel);
+    this.directionsDisplayTwo.setPanel(directionsPanel2);
 
     let start = this.origin;
     let end = this.destination;
@@ -73,8 +76,11 @@ export class MapComponent implements OnInit {
       travelMode: google.maps.TravelMode[transport],
       provideRouteAlternatives: true
     };
-    this.directionsService.route(request, (result, status) => {
+    this.directionsService.route(request, (result: google.maps.DirectionsResult, status) => {
       if (status == google.maps.DirectionsStatus.OK) {
+        this.directionsDisplayTwo.setMap(null);
+        document.getElementById("parkpromptwrapper").classList.remove("show");
+        document.getElementById("parkpromptwrapper").classList.add("hide");
         // below, finding shortest distance for driving mode of transport
         if (transport == 'DRIVING' && preference == 'SHORTEST') {
             var shortestDist = result.routes[0].legs[0].distance.value;
@@ -95,9 +101,58 @@ export class MapComponent implements OnInit {
           result.routes.length = 0;
           result.routes.push(fastestRoute);
           // console.log(result);
-          console.log(this.getClosestMetroWithParking([fastestRoute.legs["0"].start_location.lat(), fastestRoute.legs["0"].start_location.lng()]))
+          if (this.methodOfTransp == "PNR") {
+            let closestMetro = this.getClosestMetroWithParking([fastestRoute.legs["0"].start_location.lat(), fastestRoute.legs["0"].start_location.lng()])
+            this.getParkAndRideDirections(start, closestMetro, end);
+            return;
+          }
         }
         this.directionsDisplay.setDirections(result);
+      } else if (google.maps.DirectionsStatus == 'ZERO_RESULTS') {
+        // this.directionsDisplay.setDirections(null);
+        console.log("impossible");
+        document.getElementById("routeoptions").innerHTML = "Cannot get directions with the current mode of transportation"
+      }
+      else {
+        console.log("misspelled");
+        document.getElementById("routeoptions").innerHTML = "Invalid Destination or Current Location"
+      }
+    })
+  }
+
+  getParkAndRideDirections(origin, closestMetro: Station, destination) {
+    var request = {
+      origin: origin,
+      destination: closestMetro.properties.NOM_STAT,
+      travelMode: google.maps.TravelMode["DRIVING"],
+      provideRouteAlternatives: false
+    };
+    this.directionsService.route(request, (result, status) => {
+      if (status == google.maps.DirectionsStatus.OK) {
+        // below, finding shortest distance for driving mode of transport
+        this.directionsDisplay.setDirections(result);
+        var secondRequest = {
+          origin: closestMetro.properties.NOM_STAT,
+          destination: destination,
+          travelMode: google.maps.TravelMode["TRANSIT"],
+          provideRouteAlternatives: false
+        };
+        this.directionsService.route(secondRequest, (result2, status2) => {
+          if (status2 == google.maps.DirectionsStatus.OK) {
+            document.getElementById("parkpromptwrapper").classList.remove("hide");
+            document.getElementById("parkpromptwrapper").classList.add("show");
+            this.directionsDisplayTwo.setMap(this.map);
+            this.directionsDisplayTwo.setDirections(result2);
+          } else if (google.maps.DirectionsStatus == 'ZERO_RESULTS') {
+            // this.directionsDisplay.setDirections(null);
+            console.log("impossible");
+            document.getElementById("routeoptions").innerHTML = "Cannot get directions with the current mode of transportation"
+          }
+          else {
+            console.log("misspelled");
+            document.getElementById("routeoptions").innerHTML = "Invalid Destination or Current Location"
+          }
+        })
       } else if (google.maps.DirectionsStatus == 'ZERO_RESULTS') {
         // this.directionsDisplay.setDirections(null);
         console.log("impossible");
@@ -122,9 +177,11 @@ export class MapComponent implements OnInit {
     };
 
     this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplayTwo = new google.maps.DirectionsRenderer;
     this.directionsService = new google.maps.DirectionsService;
     this.map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     this.directionsDisplay.setMap(this.map);
+    this.directionsDisplayTwo.setMap(this.map);
 
     this.data.currentOrigin.subscribe(origin => this.origin=origin)   
     this.data.currentDestination.subscribe(destination => this.destination=destination) 
